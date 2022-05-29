Bahtya/hermes-termux-app

commit 8b03808294b9383443fb9c8a766409cd2b73c3dc

Fixed: Fix `Settings.ACTION_*` permission requests result callback
Adding `FLAG_ACTIVITY_NEW_TASK` will start permission activity in separate task and `onActivityResult()` will be called early in the calling activity without grant/not-grant result being actually set.

termux-shared/src/main/java/com/termux/shared/android/PermissionUtils.java
@@ -352,7 +352,6 @@ public class PermissionUtils {
         Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
         intent.addCategory("android.intent.category.DEFAULT");
         intent.setData(Uri.parse("package:" + context.getPackageName()));
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         Error error;
         if (requestCode >=0)
@@ -464,7 +463,6 @@ public class PermissionUtils {
 
         Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
         intent.setData(Uri.parse("package:" + context.getPackageName()));
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         if (requestCode >=0)
             return ActivityUtils.startActivityForResult(context, requestCode, intent);
@@ -534,7 +532,6 @@ public class PermissionUtils {
 
         Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
         intent.setData(Uri.parse("package:" + context.getPackageName()));
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         if (requestCode >=0)
             return ActivityUtils.startActivityForResult(context, requestCode, intent);

commit 92a9c36071b319a223a6889e10c4a9010c0c1f55

Fixed: Fix `Settings.ACTION_*` permission requests for non-activity contexts
This was caused by 8b038082

Closes #2769

termux-shared/src/main/java/com/termux/shared/android/PermissionUtils.java
@@ -357,6 +357,11 @@ public class PermissionUtils {
         intent.addCategory("android.intent.category.DEFAULT");
         intent.setData(Uri.parse("package:" + context.getPackageName()));
 
+        // Flag must not be passed for activity contexts, otherwise onActivityResult() will not be called with permission grant result.
+        // Flag must be passed for non-activity contexts like services, otherwise "Calling startActivity() from outside of an Activity context requires the FLAG_ACTIVITY_NEW_TASK flag" exception will be raised.
+        if (!(context instanceof Activity))
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
         Error error;
         if (requestCode >=0)
             error = ActivityUtils.startActivityForResult(context, requestCode, intent, true, false);
@@ -474,6 +479,11 @@ public class PermissionUtils {
         Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
         intent.setData(Uri.parse("package:" + context.getPackageName()));
 
+        // Flag must not be passed for activity contexts, otherwise onActivityResult() will not be called with permission grant result.
+        // Flag must be passed for non-activity contexts like services, otherwise "Calling startActivity() from outside of an Activity context requires the FLAG_ACTIVITY_NEW_TASK flag" exception will be raised.
+        if (!(context instanceof Activity))
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
         if (requestCode >=0)
             return ActivityUtils.startActivityForResult(context, requestCode, intent);
         else
@@ -548,6 +558,11 @@ public class PermissionUtils {
 
         Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
         intent.setData(Uri.parse("package:" + context.getPackageName()));
+
+        // Flag must not be passed for activity contexts, otherwise onActivityResult() will not be called with permission grant result.
+        // Flag must be passed for non-activity contexts like services, otherwise "Calling startActivity() from outside of an Activity context requires the FLAG_ACTIVITY_NEW_TASK flag" exception will be raised.
+        if (!(context instanceof Activity))
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         if (requestCode >=0)
             return ActivityUtils.startActivityForResult(context, requestCode, intent);