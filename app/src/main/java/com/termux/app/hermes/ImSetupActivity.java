beb7685bffed64b74d8f4fdb29366dd6e9bb4fd2
Bahtya <bahtyar153@qq.com>
2026-05-11 00:59:29 +0800

    fix: Fix quote escaping in ImSetupActivity JSON parsing
    Bahtya

--- app/src/main/java/com/termux/app/hermes/ImSetupActivity.java
+++ app/src/main/java/com/termux/app/hermes/ImSetupActivity.java
@@ -345,7 +345,7 @@ public class ImSetupActivity extends AppCompatActivity {
                 // Extract bot username from response
                 if (body.contains("\"username\":\"")) {
                     int start = body.indexOf("\"username\":\"") + 13;
-                    int end = body.indexOf(""", start);
+                    int end = body.indexOf("\\\"", start);
                     return new String[]{"200", "@" + body.substring(start, end)};
                 }
             }
