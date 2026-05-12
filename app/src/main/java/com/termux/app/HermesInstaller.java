6f2b8dffca66d1951b684f99b145510aeafe657a
刘威 <liuweid@adtec.com.cn>
2026-05-12 13:36:42 +0800

    fix: Add missing FileInputStream import
    Bahtya

--- app/src/main/java/com/termux/app/HermesInstaller.java
+++ app/src/main/java/com/termux/app/HermesInstaller.java
@@ -22,6 +22,7 @@ import com.termux.shared.termux.TermuxConstants;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 
